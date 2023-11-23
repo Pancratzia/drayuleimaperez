a163001e801fc3b1ec1bde1d5ce4d49566e9999e
Arthuro Dugarte <adugarte@marnagarage.com>
2023-11-23 11:30:32 -0400

    Header con navegación terminada

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,26 +11,40 @@ const Header = () => {
 
   return (
     <header className="header">
-      <div className="barra">
-        <a href="/" className="barra__imagen">
-          <img src="/img/logo.png" alt="logo" />
-        </a>
+      <div className="header__grid">
 
-        <div className={abierto ? "barra__navegacion abierto" : "barra__navegacion cerrado"} onClick={manejarClick}>
-          <span></span>
-          <span></span>
-          <span></span>
+        <div className="barra">
+          <a href="/" className="barra__logo">
+            <img src="/img/logo.png" alt="logo" />
+            <h1 className="barra__titulo">Dra. Yuleima Pérez</h1>
+          </a>
+
+          <div className={abierto ? "barra__navegacion abierto" : "barra__navegacion cerrado"} onClick={manejarClick}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </div>
-      </div>
 
-      <nav className={abierto ? "nav abierto" : "nav cerrado"}>
-        <a href="#" className="nav__enlace">Home</a>
-        <a href="#" className="nav__enlace">Sobre mi</a>
-        <a href="#" className="nav__enlace">Mis servicios</a>
-        <a href="#" className="nav__enlace">Mis trabajos</a>
-        <a href="#" className="nav__enlace">Mi ubicación</a>
-        <a href="#" className="nav__enlace">Horario</a>
-      </nav>
+        <nav className={abierto ? "nav abierto" : "nav cerrado"}>
+          <a href="#" className="nav__enlace">Home</a>
+          <a href="#" className="nav__enlace">Sobre Mí</a>
+          <a href="#" className="nav__enlace">Servicios</a>
+          <a href="#" className="nav__enlace">Trabajos</a>
+          <a href="#" className="nav__enlace">Mi ubicación</a>
+          <a href="#" className="nav__enlace">Horario</a>
+        </nav>
+
+        <nav className="nav--desktop">
+          <a href="#" className="nav--desktop__enlace">Home</a>
+          <a href="#" className="nav--desktop__enlace">Sobre Mí</a>
+          <a href="#" className="nav--desktop__enlace">Servicios</a>
+          <a href="#" className="nav--desktop__enlace">Trabajos</a>
+          <a href="#" className="nav--desktop__enlace">Ubicación</a>
+          <a href="#" className="nav--desktop__enlace">Horario</a>
+        </nav>
+
+      </div>
     </header>
   );
 };
