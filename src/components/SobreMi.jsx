1263e598da397804685baa82bb401ef64058db9b
Arthuro Dugarte <adugarte@marnagarage.com>
2023-11-27 15:36:57 -0400

    Estilos a sobre mi, en la parte de la carta y contenedor

--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -1,32 +1,52 @@
 const SobreMi = () => {
   return (
     <div className="sobremi">
-      
-      <h2 className="sobremi__titulo">
-        ¿Quién soy?
-      </h2>
+      <h2 className="sobremi__titulo">¿Quién soy?</h2>
 
       <div className="sobremi__grid">
+        <div className="sobremi__grid-contenido">
 
-        <div className="sobremi__imagen">
-          <div className="sobremi__img">
-            <img src="https://images.pexels.com/photos/8376187/pexels-photo-8376187.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Dra. Yuleima Perez"/>
+          <div className="sobremi__imagen">
+            <div className="sobremi__img">
+              <img
+                src="https://images.pexels.com/photos/8376187/pexels-photo-8376187.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="Dra. Yuleima Perez"
+              />
+            </div>
+          </div>
+
+          <div className="sobremi__contenido">
+
+            <h3 className="sobremi__h3">Dra. Yuleima Pérez</h3>
+
+            <p className="sobremi__subtitulo">
+              Especialista en Ginecología y Obstetricia
+            </p>
+
+            <p className="sobremi__texto">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+              ipsam dolores ratione maiores, quos iste minus optio nemo natus.
+              Quaerat labore, deserunt ullam atque delectus nihil nulla in
+              facere eos vero. Vel unde amet aliquam quia excepturi minima, eum
+              alias animi, itaque necessitatibus ea laudantium? Odit id itaque
+              modi, ut a corporis sed optio possimus molestiae amet sunt
+              voluptatem, ad pariatur, natus nam ex. Excepturi, placeat atque.
+              Dolore culpa, officiis illo sunt minus ratione nulla.
+            </p>
+
           </div>
         </div>
 
-        <div className="sobremi__contenido">
-          <h3 className="sobremi__h3">Dra. Yuleima Pérez</h3>
-          <p className="sobremi__subtitulo">Especialista en Ginecología y Obstetricia</p>
-          <p className="sobremi__texto">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas ipsam dolores ratione maiores, quos iste minus optio nemo natus. Quaerat labore, deserunt ullam atque delectus nihil nulla in facere eos vero. Vel unde amet aliquam quia excepturi minima, eum alias animi, itaque necessitatibus ea laudantium? Odit id itaque modi, ut a corporis sed optio possimus molestiae amet sunt voluptatem, ad pariatur, natus nam ex. Excepturi, placeat atque. Dolore culpa, officiis illo sunt minus ratione nulla.
-          </p>
-
-        </div>
+        <aside className="sobremi__aside">
+          <div className="carta">
+            <h1>Titulo</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum distinctio cum exercitationem error, nesciunt modi!</p>
+          </div>
+        </aside>
 
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default SobreMi
+export default SobreMi;
