b71947e4b68d3b1d0c9ce45cf571edcfb95a5589
Arthuro Dugarte <adugarte@marnagarage.com>
2023-11-27 11:56:13 -0400

    Comienzo de sección sobre mi

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header"
 import Hero from "./components/Hero"
 import Footer from "./components/Footer"
 import Mapa from "./components/Mapa"
+import SobreMi from "./components/SobreMi"
 
 function App() {
 
@@ -10,7 +11,7 @@ function App() {
       <Header/>
       <div className="contenido">
         <Hero />
-
+        <SobreMi />
         <Mapa />
         <Footer />
       </div>
